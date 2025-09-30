6ade0b34e79ee5170d69bbc585720657bf3a5fe4
DominiqueRhoden <drho4706@students.vsu.edu>
2025-09-30 16:53:13 -0400

    Feature: revise Ping method to pass all tests

--- MongoDBConnector.Tests/MongoDBConnector.Test.cs
+++ MongoDBConnector.Tests/MongoDBConnector.Test.cs
@@ -1,11 +1,34 @@
+using Xunit;
+using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Containers;
+using System.Threading.Tasks;
+
 namespace MongoDBConnector.Tests
 {
-    public class UnitTest1
+    public class MongoDBConnectorTests
     {
         [Fact]
-        public void Test1()
+        public async Task Ping_ShouldReturnTrue_WhenMongoIsRunning()
         {
+            var mongoContainer = new TestcontainersBuilder<TestcontainersContainer>()
+                .WithImage("mongo:6.0")
+                .WithPortBinding(27017, 27017)
+                .Build();
 
+            await mongoContainer.StartAsync();
+
+            var connector = new MongoDBConnector("mongodb://localhost:27017");
+            Assert.True(connector.Ping());
+
+            await mongoContainer.StopAsync();
         }
+
+        [Fact]
+        public void Ping_ShouldReturnFalse_WhenConnectionFails()
+        {
+            var connector = new MongoDBConnector("mongodb://invalid:27017");
+            Assert.False(connector.Ping());
+        }
+
     }
 }
--- MongoDBConnector/MongoDBConnector.cs
+++ MongoDBConnector/MongoDBConnector.cs
@@ -1,7 +1,28 @@
-﻿namespace MongoDBConnector
+﻿using MongoDB.Driver;
+
+namespace MongoDBConnector
 {
     public class MongoDBConnector
     {
+        public bool Ping()
+        {
+            try
+            {
+                _client.ListDatabaseNames(); // lightweight query
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
 
+        private readonly IMongoClient _client;
+
+        public MongoDBConnector(string connectionString)
+        {
+            _client = new MongoClient(connectionString);
+        }
     }
 }
+    
